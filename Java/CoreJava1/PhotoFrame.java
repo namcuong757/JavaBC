6324d492ce8b024ca26696ba429521540cfbb725
namcuong757 <trannamcuong2208@gmail.com>
2023-06-16 14:36:34 -0700

    update0

--- Java/CoreJava1/PhotoFrame.java
+++ Java/CoreJava1/PhotoFrame.java
@@ -1,10 +1,18 @@
 package CoreJava1;
-
+import java.util.Scanner;
 public class PhotoFrame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter 1st side: ");
+		int side1 = scanner.nextInt();
+		System.out.println("Enter 2nd side: ");
+		int side2 = scanner.nextInt();
+		
+		if(side1 == side2) System.out.println("Square");
+		else System.out.println("Rectangel");
 	}
 
 }
